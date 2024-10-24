655394191dda641a57432ea7f5df89af31c5919c
hansmbua <hansmbua@yahoo.com>
2024-10-24 16:57:55 +0200

    added basic spring structure

---
+++ src/test/java/org/example/quizifyai/QuizifyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quizifyai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizifyAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
